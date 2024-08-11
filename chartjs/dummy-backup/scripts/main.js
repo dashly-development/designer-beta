7c1f85ad9844be339c63efd4c038703393b76823
Pepijn Steijger <admin@dashly.nl>
2024-08-11 21:12:09 +0200

    main v2

--- chartjs/dummy-backup/scripts/main.js
+++
@@ -1,8 +0,0 @@
-// main.js
-import { initializeApp } from './init.js';
-import { attachEventHandlers } from './eventHandlers.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initializeApp();
-  attachEventHandlers();
-});
